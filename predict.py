f9b5f7ce99697e03cf3f9a0bf200d2cb2bb61abf
Balkı <balki1985@gmail.com>
2020-12-20 20:16:45 +0300

    Initial

--- predict.py
+++ predict.py
@@ -16,7 +16,7 @@ test_folder = "Test014"
 class Config:
     DATASET_PATH = os.path.join(working_directory, "UCSD_Anomaly_Dataset.v1p2", "UCSDped1", "Train")
     SINGLE_TEST_PATH = os.path.join(working_directory, "UCSD_Anomaly_Dataset.v1p2", "UCSDped1", "Test", test_folder)
-    BATCH_SIZE = 4
+    BATCH_SIZE = 16
     EPOCHS = 3
     MODEL_PATH = os.path.join(working_directory, "model.hdf5")
 
@@ -79,7 +79,7 @@ def evaluate(model):
         sequences[i] = clip
 
     # get the reconstruction cost of all the sequences
-    reconstructed_sequences = model.predict(sequences, batch_size=4)
+    reconstructed_sequences = model.predict(sequences, batch_size=Config.BATCH_SIZE)
     sequences_reconstruction_cost = np.array([np.linalg.norm(np.subtract(sequences[i],reconstructed_sequences[i])) for i in range(0, sz)])
     sa = (sequences_reconstruction_cost - np.min(sequences_reconstruction_cost)) / np.max(sequences_reconstruction_cost)
     sr = 1.0 - sa
