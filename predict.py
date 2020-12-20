734f90ebd6bba26d8de7f4dd0fe6638c60709a85
Balkı <balki1985@gmail.com>
2020-12-20 19:59:40 +0300

    Initial

---
+++ predict.py
@@ -0,0 +1,95 @@
+import os
+from os import listdir
+from os.path import join, isdir
+import numpy as np
+from PIL import Image
+import keras
+from keras.layers import Conv2DTranspose, ConvLSTM2D, TimeDistributed, Conv2D
+from keras.models import Sequential, load_model
+from keras_layer_normalization import LayerNormalization
+import matplotlib.pyplot as plt
+
+working_directory = '/Users/balki/files/'
+
+
+class Config:
+    DATASET_PATH = os.path.join(working_directory, "UCSD_Anomaly_Dataset.v1p2", "UCSDped1", "Train")
+    SINGLE_TEST_PATH = os.path.join(working_directory, "UCSD_Anomaly_Dataset.v1p2", "UCSDped1", "Test", "Test032")
+    BATCH_SIZE = 4
+    EPOCHS = 3
+    MODEL_PATH = os.path.join(working_directory, "model.hdf5")
+
+
+def get_model(training_set, reload_model=True):
+    if not reload_model:
+        return load_model(Config.MODEL_PATH,custom_objects={'LayerNormalization': LayerNormalization})
+
+    seq = Sequential()
+    seq.add(TimeDistributed(Conv2D(128, (11, 11), strides=4, padding="same"), batch_input_shape=(None, 10, 256, 256, 1)))
+    seq.add(LayerNormalization())
+    seq.add(TimeDistributed(Conv2D(64, (5, 5), strides=2, padding="same")))
+    seq.add(LayerNormalization())
+    # # # # #
+    seq.add(ConvLSTM2D(64, (3, 3), padding="same", return_sequences=True))
+    seq.add(LayerNormalization())
+    seq.add(ConvLSTM2D(32, (3, 3), padding="same", return_sequences=True))
+    seq.add(LayerNormalization())
+    seq.add(ConvLSTM2D(64, (3, 3), padding="same", return_sequences=True))
+    seq.add(LayerNormalization())
+    # # # # #
+    seq.add(TimeDistributed(Conv2DTranspose(64, (5, 5), strides=2, padding="same")))
+    seq.add(LayerNormalization())
+    seq.add(TimeDistributed(Conv2DTranspose(128, (11, 11), strides=4, padding="same")))
+    seq.add(LayerNormalization())
+    seq.add(TimeDistributed(Conv2D(1, (11, 11), activation="sigmoid", padding="same")))
+    print(seq.summary())
+
+    seq.compile(loss='mse', optimizer=keras.optimizers.Adam(lr=1e-4, decay=1e-5, epsilon=1e-6))
+    seq.fit(training_set, training_set,
+            batch_size=Config.BATCH_SIZE, epochs=Config.EPOCHS, shuffle=False)
+    seq.load(Config.MODEL_PATH)
+    return seq
+
+
+def get_single_test():
+    sz = 200
+    test = np.zeros(shape=(sz, 256, 256, 1))
+    cnt = 0
+    for f in sorted(listdir(Config.SINGLE_TEST_PATH)):
+        if str(join(Config.SINGLE_TEST_PATH, f))[-3:] == "tif":
+            img = Image.open(join(Config.SINGLE_TEST_PATH, f)).resize((256, 256))
+            img = np.array(img, dtype=np.float32) / 256.0
+            test[cnt, :, :, 0] = img
+            cnt = cnt + 1
+    return test
+
+
+def evaluate(model):
+    print("got model")
+    test = get_single_test()
+    print("got test")
+    sz = test.shape[0] - 10
+    sequences = np.zeros((sz, 10, 256, 256, 1))
+    # apply the sliding window technique to get the sequences
+    for i in range(0, sz):
+        clip = np.zeros((10, 256, 256, 1))
+        for j in range(0, 10):
+            clip[j] = test[i + j, :, :, :]
+        sequences[i] = clip
+
+    # get the reconstruction cost of all the sequences
+    reconstructed_sequences = model.predict(sequences, batch_size=4)
+    sequences_reconstruction_cost = np.array([np.linalg.norm(np.subtract(sequences[i],reconstructed_sequences[i])) for i in range(0, sz)])
+    sa = (sequences_reconstruction_cost - np.min(sequences_reconstruction_cost)) / np.max(sequences_reconstruction_cost)
+    sr = 1.0 - sa
+
+    # plot the regularity scores
+    plt.plot(sr)
+    plt.ylabel('regularity score Sr(t)')
+    plt.xlabel('frame t')
+    plt.show()
+
+
+if __name__ == '__main__':
+    model = get_model(None, False)
+    evaluate(model)
